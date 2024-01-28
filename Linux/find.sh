a1ae305d4fe3e66ff643e3742d962d2464a1c354
Jmilton42 <joeymilton04@gmail.com>
2024-01-27 21:07:31 -0600

    Updated find.sh moving the files else where

--- Linux/find.sh
+++ Linux/find.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-if find /path -name '.*shrc' -delete; then #This is looking for any files that has .shrc type and delete them
-    echo "Found .*shrc files!" #If their was any it will prints this output
-else
-    echo "Found nothing" #If there wasn't any it prints this output
-fi
-if find /path -name '.*profile' -delete; then #This is looking for any files that has .profile type and delete them
-    echo "Found .*profile files!" #If their was any it will prints this output
-else
-    echo "Found nothing" #If there wasn't any it prints this output
-fi
+find /root /home -name '.*rc' -exec mv {} {}.bak \;
+find /root /home -name '.*profile' -exec mv {} {}.bak \;
