f7762de3e9393b34f4399c027c3db85d512454a7
Brett Billingsley <119644669+Bbillingsley2@users.noreply.github.com>
2024-02-03 13:28:40 -0600

    Updated the DB backup script

---
+++ Linux/mysql.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Username:"
+read username
+
+echo "Password:"
+read pass
+
+if [ -z "$(dpkg -l | grep mysql)" ]; then
+	echo "MySQL is not installed, installing it now."
+	sudo apt update 
+	sudo apt install -y mysql-server
+else
+	version=$(mysql --version | awk '{print $5}') 
+	echo "MySQL is already installed, the version is $version."
+fi
+
+DUMP_DIR="/root/backups/sql"
+
+mkdir -p "$DUMP_DIR"
+
+echo "dumping all databases"
+
+mysql -u $username -p$pass --all-databases > "$DUMP_DIR/all_databases.sql"
+
+echo "Would you like to install MySQL Workbench?"
+read choice
+
+if [ "$choice" == "yes" ]; then
+	sudo apt update
+	sudo apt install mysql-workbench
+fi
