d8e9057fbdd02615b168c737dfda776780d3a278
Brett Billingsley <119644669+Bbillingsley2@users.noreply.github.com>
2024-02-03 13:50:38 -0600

    Added script to import backup databases

---
+++ Linux/import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Username:"
+read username
+
+echo "Password:"
+read pass
+
+DUMP_DIR="/root/backups/sql"
+
+if mysql -u $username -p$pass < "$DUMP_DIR/all_databases.sql"; then
+    echo "Import successful!"
+else
+    echo "Error: Import failed."
+fi
+
