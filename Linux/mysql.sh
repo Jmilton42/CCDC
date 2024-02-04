37426aa1761fff4f39455dcde946813c24ad4235
Brett Billingsley <119644669+Bbillingsley2@users.noreply.github.com>
2024-02-04 12:45:09 -0600

    Update mysql.sh

--- Linux/mysql.sh
+++ Linux/mysql.sh
@@ -4,12 +4,41 @@ echo "Username:"
 read username
 
 echo "Password:"
-read pass
+read -s pass
+
+pkg=$(which apt || which yum || which pacman || which apk || which zypper || which dnf)
 
 if [ -z "$(dpkg -l | grep mysql)" ]; then
 	echo "MySQL is not installed, installing it now."
-	sudo apt update 
-	sudo apt install -y mysql-server
+
+	if [[ -z "$pkg" ]]; then
+	    echo "[-] Could not find a package manager. Run pspy manually"
+	fi
+	
+	case $pkg in
+	    *apt*)
+	        apt install -y mysql-server
+	        ;;
+	    *yum*)
+	        yum install -y mysql-server
+	        ;;
+	    *pacman*)
+	        pacman -S --noconfirm mariadb
+	        ;;
+	    *apk*)
+	        apk add mariadb-server
+	        ;;
+	    *zypper*)
+	        zypper install -y mariadb
+	        ;;
+	    *dnf*)
+	        dnf install -y mariadb-server
+	        ;;
+	    *)
+	        echo "[-] Could not find a package manager. Run pspy manually"
+	        ;;
+	esac
+	
 else
 	version=$(mysql --version | awk '{print $5}') 
 	echo "MySQL is already installed, the version is $version."
@@ -27,6 +56,29 @@ echo "Would you like to install MySQL Workbench?"
 read choice
 
 if [ "$choice" == "yes" ]; then
-	sudo apt update
-	sudo apt install mysql-workbench
+
+	case $pkg in
+	    *apt*)
+	        apt install -y mysql-workbench
+	        ;;
+	    *yum*)
+	        yum install -y mysql-workbench
+	        ;;
+	    *pacman*)
+	        pacman -S --noconfirm mysql-workbench
+	        ;;
+	    *apk*)
+	        apk add mysql-workbench
+	        ;;
+	    *zypper*)
+	        zypper install -y mysql-workbench
+	        ;;
+	    *dnf*)
+	        dnf install -y mysql-workbench
+	        ;;
+	    *)
+	        echo "[-] Could not find a package manager. Run pspy manually"
+        ;;
+	esac
+
 fi
